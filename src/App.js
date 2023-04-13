c47423579f2bf4ab47adfcbe4d7a029f45d6eb20
Paulo <pmarquesbaronefilho18@protonmail.com>
2023-04-13 12:14:00 -0300

    fix: bugs and add: not found text

--- src/App.js
+++ src/App.js
@@ -5,13 +5,18 @@ import SearchIcon from '@mui/icons-material/SearchOutlined';
 
 function App() {
   const [countries, setCountries] = useState([]);
-  const [searchText, setSearchText] = useState('')
+  const [searchText, setSearchText] = useState('');
+  const [region, setRegion] = useState('All');
+
+  useEffect(() => {
+    filterByRegion(region);
+  }, [region]);
 
   useEffect(() => {
     try {
       fetchData();
     } catch (error) {
-      return error;
+      console.log(error);
     }
   }, []);
 
@@ -40,14 +45,15 @@ function App() {
         const data = await res.json();
         if(data.status === 404) {
           setCountries([]);
+          <p>Not Found</p>
         } else {
           setCountries(data)
         }
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
     } else {
-      fetchData()
+      filterByRegion(region);
     }
   }
 
@@ -62,9 +68,10 @@ function App() {
       <div className="px-5 md:px-24 py-10 flex flex-col gap-3 sm:flex-row sm:justify-between sm:gap-24">
         <form className="relative w-full max-w-lg" onSubmit={handleSearchCountry}>
           <SearchIcon className="absolute top-3 left-4 dark:text-white" />
+          {searchText.length >= 1 && countries.length === 0 && <p className="text-rose-400 absolute bottom-12">Not Found</p>}
           <input className="w-full bg-white rounded-lg outline-none py-3 pl-16 px-6 shadow-lg sm:w-full dark:bg-darkModeElements dark:text-white" placeholder="Search for a country" id="search" value={searchText} onChange={(e) => setSearchText(e.target.value)} />
         </form>
-        <select className="bg-white cursor-pointer shadow-lg outline-none py-3 px-3 rounded-lg w-36 dark:bg-darkModeElements dark:text-white" onChange={(e) => filterByRegion(e.target.value)}>
+        <select className="bg-white cursor-pointer shadow-lg outline-none py-3 px-3 rounded-lg w-36 dark:bg-darkModeElements dark:text-white" onChange={(e) => setRegion(e.target.value)}>
           <option>All</option>
           <option>Africa</option>
           <option>Americas</option>
